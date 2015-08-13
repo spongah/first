035a1f4fa51b6d97ab913cbd782cf7ce179bbb9c
spongah <rocksolid801@gmail.com>
2015-08-12 23:17:45 -0600

    fixed misspelled word

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
   	@header_adj = [:irie, :ital, :rasta, :rootical].sample.capitalize
   	@commandment = ['Jah say, your bomboclaat band name should be ',
-  		'Marcus Garvey prophecied that you would call your band ',
+  		'Marcus Garvey prophesied that you would call your band ',
   		'Selassie say: name your band ',
   		'The conquering lion of the tribe of judah, "JAH, RASTAFARI" thinks you should name your band '].sample
   	@first_word = [:natural, :dirty, :green, :rebel, :salty, :rankin, :afro, :tribal]
