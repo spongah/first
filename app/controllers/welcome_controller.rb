d33404d4d46cc149c32562fbed67892c10ed3e14
spongah <rocksolid801@gmail.com>
2015-08-12 22:17:34 -0600

    data is in controller now

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+  	@first_word = [:natural, :dirty, :green, :rebel, :salty, :rankin, :afro, :tribal]
+	@second_word = [:seeds, :roots, :trees, :redemption, :vibrations, :lion, :zion]
+	@bandname = @first_word.sample.to_s + " " + @second_word.sample.to_s
   end
 end
